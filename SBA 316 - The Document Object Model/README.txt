bb0e9058a6e46984eb23aabe1cdbdfc0576348b3
coleman7245 <dlcoleman1@live.com>
2024-12-18 14:58:39 -0700

    Added setChessPieces function, chess images, and README.

---
+++ SBA 316 - The Document Object Model/README.txt
@@ -0,0 +1,5 @@
+Chess Game in JS, CSS, and HTML
+by Derek Coleman
+
+NOTE: Chess images from Wiki Commons
+
--- SBA 316 - The Document Object Model/scripts/SBA316.js
+++ SBA 316 - The Document Object Model/scripts/SBA316.js
@@ -1,24 +1,51 @@
+const defaultChessPiecePos = ["("];
 let body = document.body;
 
-function createChessBoard(body) {
+function createChessBoard() {
     let board = document.createElement("div");
     board.setAttribute("id", "board");
-    board.style.margin = "25% 50% 0 50%";
     let square = null;
     let color = "";
 
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
             square = document.createElement("div");
-            square.style.width = "25px";
-            square.style.height = "25px";
+            square.setAttribute("value", `(${row+1}, ${col+1})`);
+            square.style.width = "75px";
+            square.style.height = "75px";
             square.style.backgroundColor = color;
-            color = (color === "rgb(0, 0, 0)") ? "rgb(255, 255, 255)" : color;
+            color = (color === "black") ? "white" : "black";
             board.append(square);
         }
+
+        color = (color === "black") ? "white" : "black";
     }
 
-    body.append(board);
+    return board;
 }
 
-createChessBoard(body);
+function setChessPieces (board, color) {
+    let chessPieces = [];
+    let chessPiece = null;
+    let image = null;
+    let source = (color === "black") ? "../images/chess_black_pawn.png" : "../images/chess_white_pawn.png";
+
+    for (let n = 0; n < 8; n++) {
+        chessPiece = document.createElement("div");
+        chessPiece.style.zIndex = -1;
+        image = document.createElement("img");
+        image.setAttribute("src", source);
+        chessPiece.appendChild(image);
+        chessPiece.setAttribute("class", "pawn");
+        chessPieces.push(chessPiece);
+    }
+
+    return chessPieces;
+}
+
+body.append(createChessBoard());
+
+console.log(setChessPieces(board));
+
+
+
